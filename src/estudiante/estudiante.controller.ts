14da9019a142c56382f3e92efbbd0b571f01c050
Andy Ortiz <andyortiz93@gmail.com>
2025-05-18 15:47:30 -0500

    adds estudiante endpoints

--- src/estudiante/estudiante.controller.ts
+++ src/estudiante/estudiante.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch } from '@nestjs/common';
 import { EstudianteService } from './estudiante.service';
 import { CreateEstudianteDto } from './dto/create-estudiante.dto/create-estudiante.dto';
 import { Estudiante } from './estudiante.entity';
@@ -7,6 +7,11 @@ import { Estudiante } from './estudiante.entity';
 export class EstudianteController {
   constructor(private readonly estudianteService: EstudianteService) {}
 
+  @Post()
+  async crearEstudiante(@Body() dto: CreateEstudianteDto): Promise<Estudiante> {
+    return await this.estudianteService.crearEstudiante(dto);
+  }
+
   @Get(':id')
   async findEstudianteById(
     @Param('id') id: number,
@@ -14,8 +19,11 @@ export class EstudianteController {
     return await this.estudianteService.findEstudianteById(id);
   }
 
-  @Post()
-  async crearEstudiante(@Body() dto: CreateEstudianteDto): Promise<Estudiante> {
-    return await this.estudianteService.crearEstudiante(dto);
+  @Patch('inscribirse/:id/actividad/:actividadId')
+  async inscribirseEnActividad(
+    @Param('id') id: number,
+    @Param('actividadId') actividadId: number,
+  ): Promise<Estudiante> {
+    return await this.estudianteService.inscribirseActivdad(id, actividadId);
   }
 }
