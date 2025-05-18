4549528e91c2f807d164aadc9726bebb73485834
Andy Ortiz <andyortiz93@gmail.com>
2025-05-18 15:18:16 -0500

    adds missing import

--- src/estudiante/estudiante.module.ts
+++ src/estudiante/estudiante.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Estudiante } from './estudiante.entity';
 import { EstudianteService } from './estudiante.service';
 import { EstudianteController } from './estudiante.controller';
+import { Actividad } from '../actividad/actividad.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Estudiante])],
+  imports: [TypeOrmModule.forFeature([Estudiante, Actividad])],
   providers: [EstudianteService],
   controllers: [EstudianteController],
   exports: [EstudianteService],
